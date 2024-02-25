733b58b0f4d06fba9c0ccf00e5376785d0c674b5
Mouad Bennani <mouadgaming3@gmail.com>
2024-02-25 01:03:01 +0100

    inception mandatory 80% done

---
+++ srcs/requirements/mariadb/config/wordpressdb.sql
@@ -0,0 +1,9 @@
+USE mydatabase;
+
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+INSERT INTO users (username, password) VALUES ('admin', 'password');
---
+++ srcs/requirements/wordpress/config/wordpress.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sleep 10
+cd /var/www/wordpress
+
+wp config create --dbhost=$DB_HOST --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --path=/var/www/wordpress --allow-root
+
+wp core install --title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_MAIL \
+				--url=$WP_URL \
+				--path=/var/www/wordpress \
+				--allow-root
+
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --path=/var/www/wordpress --allow-root
+cd -
+
+php-fpm7.3 -F
